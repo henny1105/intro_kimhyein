e6cf4eb966948bd09dc34b18cabc300392f745bf
kimhye1105 <kimhyein1105@gmail.com>
2021-08-05 20:54:24 +0900

    자기소개서 페이지 업로드

---
+++ main.js
@@ -0,0 +1,48 @@
+var $btns = $("#navi li"); 
+var $boxs = $(".myScroll"); 
+var len = $btns.length; 
+var posArr = []; 
+var baseLine = -300; 
+
+setPos(); 
+
+$(window).on("resize", setPos);
+
+$btns.children("a").on("click", function(e){
+    e.preventDefault(); 
+    moveScroll(this);      
+});
+
+$(window).on("scroll", function(){
+    var scroll = $(this).scrollTop(); 
+
+   activateBtn(scroll);   
+ });
+
+function setPos(){
+    posArr=[]; 
+
+    for(var i=0; i<len; i++){
+        posArr.push($boxs.eq(i).offset().top); 
+    }   
+}
+
+function activateBtn(scroll){   
+    for(var i=0; i<len; i++){
+       
+        if(scroll >= posArr[i] + baseLine){     
+            $btns.children("a").removeClass("on");           
+            $btns.eq(i).children("a").addClass("on"); 
+            $boxs.eq(i).addClass("on");
+        }
+    }  
+ }
+
+function moveScroll(el){
+    var target = $(el).attr("href");
+    var targetPos = $(target).offset().top; 
+ 
+     $("html, body").animate({
+         scrollTop: targetPos
+     }, 1000); 
+}
